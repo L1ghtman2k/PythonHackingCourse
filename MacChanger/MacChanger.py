e0427bbc57ee276e3cbc1e2da9988caa61d52b6d
l1ghtman2k <aibek.zhil@gmail.com>
2018-12-31 01:21:44 -0500

    Mac Changer using better syntax

--- MacChanger/MacChanger.py
+++ MacChanger/MacChanger.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 import subprocess
 import re
+import netifaces
 
-inp = input("input mac > ")
+macAddress = input("input mac > ")
+interface = input("input interface > ")
+
 pattern = re.compile("^([0-9a-f]{2}[:-]){5}([0-9a-f]{2})$", re.IGNORECASE)
-if pattern.match(inp):
-    subprocess.call("ifconfig eth0 down", shell=True)
-    subprocess.call(f"ifconfig eth0 hw ether {inp}", shell=True)
-    subprocess.call("ifconfig eth0 up", shell=True)
+
+if pattern.match(macAddress) and netifaces.AF_INET in netifaces.ifaddresses(interface):
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", macAddress])
+    subprocess.call(["ifconfig", interface, "down"])
+
 else:
-    print("Bad value")
+    raise ValueError ("You must specify a valid Mac Address.")
+
+# older version:
+# if pattern.match(MacAddress):
+#
+#     subprocess.call("ifconfig eth0 down", shell=True)
+#     subprocess.call(f"ifconfig eth0 hw ether {MacAddress}", shell=True)
+#     subprocess.call("ifconfig eth0 up", shell=True)
+#
+# else:
+#     print("Bad value")
 # DEFAULT MAC 08:00:27:07:2e:d3"
