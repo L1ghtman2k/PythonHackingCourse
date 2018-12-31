faeee24fc94ab969ad674cdb11450fdc16747e05
l1ghtman2k <aibek.zhil@gmail.com>
2018-12-31 01:31:26 -0500

    Removed unnecessary try, catches

--- MacChanger/MacChanger.py
+++ MacChanger/MacChanger.py
@@ -6,16 +6,14 @@ import netifaces
 macAddress = input("input mac > ")
 interface = input("input interface > ")
 
-pattern = re.compile("^([0-9a-f]{2}[:-]){5}([0-9a-f]{2})$", re.IGNORECASE)
-
+pattern = re.compile(r"^([0-9a-f]{2}[:-]){5}([0-9a-f]{2})$", re.IGNORECASE)
 if pattern.match(macAddress) and netifaces.AF_INET in netifaces.ifaddresses(interface):
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", macAddress])
-    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "up"])
 
 else:
-    raise ValueError ("You must specify a valid Mac Address.")
+    raise ValueError("You must specify a valid Mac Address.")
 
 # older version:
 # if pattern.match(MacAddress):
