482809352097e23fd2fc2ebb6d2699e3fdcd43a8
l1ghtman2k <aibek.zhil@gmail.com>
2019-01-04 14:13:08 -0500

    Adding Pretty Output using template

--- Network_Scanner/NetworkScanner.py
+++ Network_Scanner/NetworkScanner.py
@@ -5,9 +5,14 @@ def scan(ip):
     arp_req = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     final_packet = broadcast/arp_req
-    (answered, not_answered) = scapy.srp(final_packet, timeout=1)
-    print(answered.summary())
+    # answered, not_answered = scapy.srp(final_packet, timeout=1)
+    answered = scapy.srp(final_packet, timeout=1, verbose=False)[0]
 
+    template = "{0:8}  |  {1:10}"  # column widths:
+    print (template.format("IP", "MAC Address"))  # header
+    print("------------------------------")
+    for element in answered:
+        print(template.format(element[1].psrc,element[1].hwsrc))
 
     # to get the summary of a packet use : packet.summary() or packet.show()
     # to get more info use : scapy.ls(type)
