895b29d0cf453aa1b2187dbd0c7261b4f745a659
l1ghtman2k <aibek.zhil@gmail.com>
2019-01-04 14:23:45 -0500

    Added command line arguments instead of manual user input

--- Network_Scanner/NetworkScanner.py
+++ Network_Scanner/NetworkScanner.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+import optparse
 
 
 def scan(ip):
@@ -18,5 +19,13 @@ def scan(ip):
     # to get more info use : scapy.ls(type)
 
 
-ip = input("Please Enter IP address or a subnet to scan: ")
-scan(ip)
+def pars():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target ", dest="target", help="A single target or a subnet to be scanned")
+    return parser.parse_args()
+
+
+options, arguments = pars()
+target = options.target
+# ip = input("Please Enter IP address or a subnet to scan: ")
+scan(target)
